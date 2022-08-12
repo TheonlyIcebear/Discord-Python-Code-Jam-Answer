f052e6e00cee8060b21b64e8645ba11be1ff024d
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-08-12 03:23:24 -0400

    Add files via upload

---
+++ qualifier/main.py
@@ -0,0 +1,47 @@
+import typing, asyncio, random, string
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Request:
+    scope: typing.Mapping[str, typing.Any]
+
+    receive: typing.Callable[[], typing.Awaitable[object]]
+    send: typing.Callable[[object], typing.Awaitable[None]]
+
+
+class RestaurantManager:
+    def __init__(self):
+        """Instantiate the restaurant manager.
+        This is called at the start of each day before any staff get on
+        duty or any orders come in. You should do any setup necessary
+        to get the system working before the day starts here; we have
+        already defined a staff dictionary.
+        """
+        self.staff = {}
+        self.registerWorkers()
+
+
+    async def __call__(self, request: Request):
+        """Handle a request received.
+        This is called for each request received by your application.
+        In here is where most of the code for your system should go.
+        :param request: request object
+            Request object containing information about the sent
+            request to your application.
+        """
+        request = request.scope
+        if request["type"] == "staff.onduty":
+            self.staff[request["id"]] = request
+
+        elif request["type"] == "staff.offduty":
+            del self.staff[request["id"]]
+
+        elif request["type"] == "order":
+            full_order = await request.recieve()
+            speciality_needed = request["speciality"] 
+            for worker in self.staff:
+                if speciality_needed in worker.scope["speciality"]:
+                    selected_worker = worker
+                    break
+            selected_worker.send(full_order)
